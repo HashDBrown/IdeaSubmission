ec381572dec8d2b00a8d2c0ddbffced7f16dcce0
HashDBrown <halkhateeb704@gmail.com>
2024-08-05 05:23:15 -0400

    Successfully connected to mySQL

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path'); // Path module
 
 const app = express();
-const port = 3001;
+const port = 3000; // Change this to an available port if 3306 is occupied
 
 app.use(cors());
 app.use(express.json());
 
+// console.log(process.env.DB_HOST);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_NAME);
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'HashDBrown',
-  password: 'Mewtwo2003S',
-  database: 'HashIdeas'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'SSL', 'DigiCertGlobalRootCA.crt.pem')) // Adjust the path accordingly
+  }
 });
 
 db.connect(err => {
@@ -21,4 +32,8 @@ db.connect(err => {
     return;
   }
   console.log('Connected to MySQL');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
