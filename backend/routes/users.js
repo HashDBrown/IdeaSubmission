17aa4a24034b826a53492d6f9ad024a385dc0b34
HashDBrown <halkhateeb704@gmail.com>
2024-08-06 16:20:33 -0400

    login endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -26,7 +26,7 @@ router.delete('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const users = await userService.createUser(req.body);
-        
+
         res.status(201).json({
             message: `User ${req.body.username} created`,
             email: users.email,
@@ -37,5 +37,23 @@ router.post('/', async (req, res) => {
         res.status(500).send(err.message);
     }
 })
+
+//login
+router.post('/login', async (req, res) => {
+    try {
+        const result = await userService.login(req.body);
+        if (result.success) {
+            res.status(200).json({
+                message: `User ${result.user.username} logged in`,
+                email: result.user.email,
+                type: result.user.type
+            });
+        } else {
+            res.status(401).json({ message: result.message });
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
 module.exports = router;
--- backend/services/users.js
+++ backend/services/users.js
@@ -14,23 +14,42 @@ async function getAllUsers() {
 }
 
 //delete user by id
-async function deleteUser(id){
+async function deleteUser(id) {
     const connection = await getConnection();
     const [rows] = await connection.query('DELETE FROM users WHERE id = ?', [id]);
     await connection.end();
     return rows;
 }
 
-async function createUser(user){
+async function createUser(user) {
     const connection = await getConnection();
     const [rows] = await connection.query('INSERT INTO users (username, password, email, type) VALUES (?, ?, ?, ?)',
-         [user.username, user.password, user.email, user.type]);
+        [user.username, user.password, user.email, user.type]);
     await connection.end();
     return rows;
+}
+
+async function login(user) {
+    const connection = await getConnection();
+    //login in with username or email
+    const [rows] = await connection.query(
+        'SELECT * FROM users WHERE (username = ? OR email = ?) AND password = ?',
+        [user.username || user.email, user.username || user.email, user.password]
+    );
+    await connection.end();
+    // Check if user exists
+    if (rows.length > 0) {
+        // Return user details excluding password
+        const { password, ...userData } = rows[0];
+        return { success: true, user: userData };
+    } else {
+        return { success: false, message: 'Invalid username/email or password' };
+    }
 }
 
 module.exports = {
     getAllUsers,
     deleteUser,
-    createUser
+    createUser,
+    login
 };
